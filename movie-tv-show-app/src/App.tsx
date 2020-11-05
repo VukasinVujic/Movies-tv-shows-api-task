f18d21477e78a259fd701585b422ad11c82c51e5
VukasinVujic <vukasinvujic021@gmail.com>
2020-11-05 22:33:59 +0100

    Adding detail card just for testing

--- movie-tv-show-app/src/App.tsx
+++ movie-tv-show-app/src/App.tsx
@@ -39,7 +39,11 @@ function App() {
           image="https://opusteno.rs/slike/2006/09/najvise-nervira-na-netu-841/najvise-nervira-na-netu-tv.jpg"
         />
       </div>
-      {/* <CardDetail /> */}
+      <CardDetail
+        title="Najjači film ove godine"
+        image="https://opusteno.rs/slike/2006/09/najvise-nervira-na-netu-841/najvise-nervira-na-netu-tv.jpg"
+        text="Fusce commodo.  Vestibulum convallis, lorem a tempus semper, dui dui euismod elit, vitae placerat urna tortor vitae lacus.  Nullam libero mauris, consequat quis, varius et, dictum id, arcu.  Mauris mollis tincidunt felis.  Aliquam feugiat tellus ut neque.  Nulla facilisis, risus a rhoncus fermentum, tellus tellus lacinia purus, et dictum nunc justo sit amet elit.  Aliquam erat volutpat.  Nunc eleifend leo vitae magna.  In id erat non orci commodo lobortis.  Proin neque massa, cursus ut, gravida ut, lobortis eget, lacus.  Sed diam.  Praesent fermentum tempor tellus."
+      />
     </div>
   );
 }
--- movie-tv-show-app/src/components/CardDetail.tsx
+++ movie-tv-show-app/src/components/CardDetail.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 
-const CardDetail = () => {
+interface Props {
+  title: string;
+  image: string;
+  text: string;
+}
+
+const CardDetail = (props: Props) => {
   return (
-    <div>
-      <p>Card detail</p>
+    <div className="cardDetailOuter">
+      <button> Back </button>
+      <div className="cardDetail">
+        <img alt="" src={props.image}></img>
+        <p className="title">{props.title}</p>
+        {/*<p> Overview </p>*/}
+        <p>{props.text}</p>
+      </div>
     </div>
   );
 };
